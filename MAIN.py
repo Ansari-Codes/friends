a7878c24bc09bc142faabe2266b1c2f43a61364e
Ansari-Codes <ansaricodes@gmail.com>
2025-11-02 21:31:05 +0500

    Fixed

--- MAIN.py
+++ MAIN.py
@@ -1,7 +1,10 @@
 from ENV import FAVICON, APP_NAME, SECRET
 import routes, ENV
 from nicegui import ui
+from db.migrate import apply_migrations
+import asyncio
 
+asyncio.run(apply_migrations())
 ui.run(
     favicon=FAVICON, 
     title=APP_NAME,
