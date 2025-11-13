1fa5da883ce17bff1489914280e80efd1a478451
Muhammad Abubakar Siddique Ansari <ansaricodes@gmail.com>
2025-11-13 21:09:27 +0500

    Add logging and increase HTTP client timeout

--- MAIN.py
+++ MAIN.py
@@ -1,3 +1,11 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,  # or DEBUG for more verbose
+    format="%(asctime)s [%(levelname)s] %(message)s"
+)
+logger = logging.getLogger(__name__)
+
 # MIGRATIONS
 from db.Migrations.CreateTableChat import up as ccup
 from db.Migrations.CreateTableUsers import up as ctup
@@ -7,9 +15,10 @@ API_URL = "http://worldofansari.com/dbapi"
 payload = {"query": sql, "to_fetch": False, "name": "friends"}
 try:
     logger.info("Applying migrations...")
-    with httpx.Client(timeout=10) as client:
+    with httpx.Client(timeout=20) as client:
         response = client.post(API_URL, json=payload)
         response.raise_for_status()
+    logger.info("Applied migrations successfully")
 except httpx.HTTPStatusError as e:
     logger.error(f"HTTP error while calling API: {e.response.status_code} - {e.response.text}")
     raise
@@ -19,8 +28,6 @@ except httpx.RequestError as e:
 except Exception as e:
     logger.exception("Unexpected error in RUN_SQL")
     raise
-finally:
-    logger.info("Applied migrations")
 
 # MAIN APP
 from ENV import FAVICON, APP_NAME, SECRET
